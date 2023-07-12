e689f3c1b8ef49777a5adad21fc70bfcde6ed692
kamashige <kamashige@n-create.co.jp>
2023-07-12 17:19:50 +0900

    3rd

--- sample/src/App.tsx
+++ sample/src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const url = 'http://127.0.0.1/api/user';
+  const [loading, setLoading] = useState(true);
+  const [recs, setRecs] = useState([]);
+
+  const Card = (data: any) => {
+    return data.items.map((item: any) => (<div>{item.name}</div>));
+  };
+
+  const execSearchName = (e: any) => {
+    if (e.target?.value?.length) {
+      getApi(url + '?name=' + e.target.value);
+    }
+  }
+
+  const getApi = (tmpUrl: string) => {
+    fetch(tmpUrl)
+      .then(res => res.json())
+      .then((json: any) => {
+        setRecs(json.res);
+        setLoading(false);
+      });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input onChange={execSearchName} type="text"/>
+        {
+          loading
+            ?
+              (
+                <img src={logo} className="App-logo" alt="logo" />
+              )
+            :
+              (
+                <div>
+                  <Card items={recs} />
+                </div>
+              )
+        }
       </header>
     </div>
   );
